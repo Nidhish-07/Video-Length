fe14dbe98d9eba7e8ac703012dfdef3273171a27
Nidhish Chauhan <rch246chauhan@gmail.com>
2023-02-18 15:03:28 +0530

    html added

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-import electron from "electron";
+const electron = require("electron");
+
+const { app, BrowserWindow } = electron;
+
+app.on("ready", () => {
+  //   console.log("Up and running");
+  const mainWindow = new BrowserWindow({});
+
+  mainWindow.loadURL(`file://${__dirname}/index.html`);
+});
